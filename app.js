d5d4b654fd7ec0027e0b669cb29676b6395a7cd1
Silver <samsilver@FVFZMXVMJ1WK.tld>
2022-03-28 14:53:13 -0500

    yes

---
+++ app.js
@@ -0,0 +1,22 @@
+function main(){
+    fname()
+}
+
+
+function fname(){
+    var name = prompt("What is thy name?")
+    alert(`Hello sir ${name}`)
+    return name
+}
+
+
+
+
+
+
+
+
+
+
+
+main()
